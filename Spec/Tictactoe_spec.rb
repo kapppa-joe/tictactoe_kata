06d47d9adb8237b0f119acef2fe0dab10bcaff10
Ted <ted.booton@madetech.com>
2022-02-14 16:34:38 +0000

    adding interactive user input

--- Spec/Tictactoe_spec.rb
+++ Spec/Tictactoe_spec.rb
@@ -1,15 +1,26 @@
 require 'tictactoe'
 
-describe "" do
-#   it "return_false_at_start_of_game" do
-#     expect(Tictactoe.new.confirm_winner('')).to eql false
-#   end
+describe "tictactoe#confirm_winner" do
+  it "return_false_at_start_of_game" do
+    expect(Tictactoe.new.confirm_winner('000000000')).to eql false
+  end
+
+  it "return_false when no player has won yet" do
+    expect(Tictactoe.new.confirm_winner('101202101')).to eql false
+    expect(Tictactoe.new.confirm_winner('121212210')).to eql false
+  end
+
+  it "return true when a player wins by vertical / horizontal / diagonal" do
+    expect(Tictactoe.new.confirm_winner('111121221')).to eql true
+    expect(Tictactoe.new.confirm_winner('122102101')).to eql true
+    expect(Tictactoe.new.confirm_winner('100010001')).to eql true
+  end
 
   it "return_true_when_a_player_occupied_a_horizontal_row" do
     expect(Tictactoe.new.confirm_horizontal_winner('111121221')).to eql true
     expect(Tictactoe.new.confirm_horizontal_winner('222121121')).to eql true
-    expect(Tictactoe.new.confirm_horizontal_winner('121111')).to eql true
-    expect(Tictactoe.new.confirm_horizontal_winner('121222')).to eql true
+    expect(Tictactoe.new.confirm_horizontal_winner('121111000')).to eql true
+    expect(Tictactoe.new.confirm_horizontal_winner('121222000')).to eql true
     expect(Tictactoe.new.confirm_horizontal_winner('121121111')).to eql true
   end
 
@@ -24,6 +35,39 @@ describe "" do
     expect(Tictactoe.new.confirm_diagonal_winner('102020201')).to eql true
     
   end
-  
+end
 
+describe "display grid" do
+  it "display an empty grid when given a string '000000000' " do
+    expect(Tictactoe.new.display_grid('000000000')).to eql "   \n   \n   "
+  end
+
+  it "display player one as X' " do
+    expect(Tictactoe.new.display_grid('100100100')).to eql "X  \nX  \nX  "
+  end
+
+  it "display player two as O' " do
+    expect(Tictactoe.new.display_grid('200020002')).to eql "O  \n O \n  O"
+  end
+end
+
+
+describe "user input requirements" do
+  it "check input for validity" do
+    expect(Tictactoe.new.check_input("1X")).to eql "100000000"
+  end
+end
+
+require "stringio"
+
+describe "ask for user input" do
+    it "record moves" do
+      input = StringIO.new("1\n")
+      output = StringIO.new
+      tictactoe = Tictactoe.new(input, output)
+
+      tictactoe.ask_for_user_input
+
+      expect(output).to eql "please input your next move ( a number from 1 to 9 )\n1"
+    end
 end
--- lib/Tictactoe.rb
+++ lib/Tictactoe.rb
@@ -1,4 +1,9 @@
 class Tictactoe
+    def initialize(input: $stdin, output: $stdout)
+        @input = input
+        @output = output
+    end
+
     def confirm_horizontal_winner(moves)
         if moves[0, 3] == "111" or moves[0, 3] == "222"
             return true
@@ -7,8 +12,6 @@ class Tictactoe
         elsif moves[6, 3] == "111" or moves[6, 3] == "222"
             return true
         end
-
-        
         false
     end
 
@@ -19,20 +22,39 @@ class Tictactoe
             return true
         elsif moves[2] + moves[5] + moves[8] == "111" or moves[2] + moves[5] + moves[8] == "222"
             return true
-        
         end
-
-    
-    
+        false
     end
+
     def confirm_diagonal_winner(moves)
         if moves[0] + moves[4] + moves[8] == "111" or moves[0] + moves[4] + moves[8] == "222"
             return true
-        
-        
+        elsif moves[2] + moves[4] + moves[6] == "111" or moves[2] + moves[4] + moves[6] == "222"
+            return true
         end
+        false
     end
 
+    def confirm_winner(moves)
+        confirm_horizontal_winner(moves) or confirm_diagonal_winner(moves) or confirm_vertical_winner(moves) 
+    end
 
+    def display_grid(moves)
+        # "   \n   \n   "
+        player1x = moves.gsub("0", " ").gsub("1","X").gsub("2","O")
+        with_newlines = player1x[0, 3] + "\n" + player1x[3, 3] + "\n" + player1x[6, 3]
+    end
+
+    def user_input(moves)
+        board = 000000000
+
+    end
+
+    def ask_for_user_input
+        puts "please input your next move ( a number from 1 to 9 )"
+        input = gets.chomp
+        puts input
+    end
 end
 
+# Tictactoe.new.ask_for_user_input
