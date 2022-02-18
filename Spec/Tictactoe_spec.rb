7e08790f6c143bed0a5ffebd6c892184fe7d5ad0
Joe Fong <joe.fong@madetech.com>
2022-02-18 15:29:55 +0000

    add a line to start_game test

--- Spec/Tictactoe_spec.rb
+++ Spec/Tictactoe_spec.rb
@@ -1,22 +1,22 @@
 require 'tictactoe'
 
-describe "tictactoe#confirm_winner" do
-  it "return_false_at_start_of_game" do
+describe 'tictactoe#confirm_winner' do
+  it 'return_false_at_start_of_game' do
     expect(Tictactoe.new.confirm_winner('000000000')).to eql false
   end
 
-  it "return_false when no player has won yet" do
+  it 'return_false when no player has won yet' do
     expect(Tictactoe.new.confirm_winner('101202101')).to eql false
     expect(Tictactoe.new.confirm_winner('121212210')).to eql false
   end
 
-  it "return true when a player wins by vertical / horizontal / diagonal" do
+  it 'return true when a player wins by vertical / horizontal / diagonal' do
     expect(Tictactoe.new.confirm_winner('111121221')).to eql true
     expect(Tictactoe.new.confirm_winner('122102101')).to eql true
     expect(Tictactoe.new.confirm_winner('100010001')).to eql true
   end
 
-  it "return_true_when_a_player_occupied_a_horizontal_row" do
+  it 'return_true_when_a_player_occupied_a_horizontal_row' do
     expect(Tictactoe.new.confirm_horizontal_winner('111121221')).to eql true
     expect(Tictactoe.new.confirm_horizontal_winner('222121121')).to eql true
     expect(Tictactoe.new.confirm_horizontal_winner('121111000')).to eql true
@@ -24,20 +24,19 @@ describe "tictactoe#confirm_winner" do
     expect(Tictactoe.new.confirm_horizontal_winner('121121111')).to eql true
   end
 
-  it "return true when a player occupied a vertical column" do
+  it 'return true when a player occupied a vertical column' do
     expect(Tictactoe.new.confirm_vertical_winner('122102101')).to eql true
     expect(Tictactoe.new.confirm_vertical_winner('022122121')).to eql true
     expect(Tictactoe.new.confirm_vertical_winner('001121121')).to eql true
   end
 
-  it "return true when a player occupied a diagonal column" do
+  it 'return true when a player occupied a diagonal column' do
     expect(Tictactoe.new.confirm_diagonal_winner('100010001')).to eql true
     expect(Tictactoe.new.confirm_diagonal_winner('102020201')).to eql true
-    
   end
 end
 
-describe "display grid" do
+describe 'display grid' do
   it "display an empty grid when given a string '000000000' " do
     expect(Tictactoe.new.display_grid('000000000')).to eql "   \n   \n   "
   end
@@ -51,90 +50,85 @@ describe "display grid" do
   end
 end
 
-describe "check_input" do
-  it "return true if game_board is empty and player selects 1" do
-    game_board = "000000000"
+describe 'check_input' do
+  it 'return true if game_board is empty and player selects 1' do
+    game_board = '000000000'
     expect(Tictactoe.new.check_input(game_board, 1)).to eql true
   end
 
-  it "return false if game_board is occupied at position 1 and player selects 1" do
-    game_board = "200000000"
+  it 'return false if game_board is occupied at position 1 and player selects 1' do
+    game_board = '200000000'
     expect(Tictactoe.new.check_input(game_board, 1)).to eql false
   end
 
-  it "return false if invalid move made outside of 1-9 range" do
-    game_board = "110000000"
+  it 'return false if invalid move made outside of 1-9 range' do
+    game_board = '110000000'
     expect(Tictactoe.new.check_input(game_board, 11)).to eql false
     expect(Tictactoe.new.check_input(game_board, 0)).to eql false
     expect(Tictactoe.new.check_input(game_board, -5)).to eql false
-    expect(Tictactoe.new.check_input(game_board, "abc")).to eql false
-  end
-
-end
-
-describe "ask for user input" do
-  it "record game_board" do
-    tictactoe = Tictactoe.new
-
-    allow(tictactoe).to receive(:gets).and_return("1")
-
-    expect{ tictactoe.ask_for_user_input }.to output("please input your next move ( a number from 1 to 9 )\n").to_stdout
-
+    expect(Tictactoe.new.check_input(game_board, 'abc')).to eql false
   end
 end
 
-
-describe "insert move" do
-  it "insert 1 to the 5th position of the board if player1 select 5 as their move" do
+describe 'ask for user input' do
+  it 'record game_board' do
     tictactoe = Tictactoe.new
-    game_board_before = "000000000"    
+
+    allow(tictactoe).to receive(:gets).and_return('1')
+
+    expect do
+      tictactoe.ask_for_user_input
+    end.to output("please input your next move ( a number from 1 to 9 )\n").to_stdout
+  end
+end
+
+describe 'insert move' do
+  it 'insert 1 to the 5th position of the board if player1 select 5 as their move' do
+    tictactoe = Tictactoe.new
+    game_board_before = '000000000'
     game_board_after = tictactoe.insert_move(game_board_before, 5)
-    expect(game_board_after).to eql "000010000"
-
-    
+    expect(game_board_after).to eql '000010000'
   end
 end
 
-describe "invalid input message" do
-  it "accept player input if gameboard is empty, and then give back the user input" do
-    game_board = "000000000"
+describe 'invalid input message' do
+  it 'accept player input if gameboard is empty, and then give back the user input' do
+    game_board = '000000000'
     player_input = 1
     tictactoe = Tictactoe.new
-    allow(tictactoe).to receive(:gets).and_return("1")
+    allow(tictactoe).to receive(:gets).and_return('1')
     expect(tictactoe.check_for_valid_move(game_board)).to eql player_input
   end
 
-  it "if player input is not valid, ask player to input again" do
-    game_board = "000000000"
+  it 'if player input is not valid, ask player to input again' do
+    game_board = '000000000'
     player_input = 10
     tictactoe = Tictactoe.new
-    allow(tictactoe).to receive(:gets).and_return("10", "1")
+    allow(tictactoe).to receive(:gets).and_return('10', '1')
 
-    expected_message = a_string_including("Sorry, your input was not valid. Can you input again?")
+    expected_message = a_string_including('Sorry, your input was not valid. Can you input again?')
 
-    expect{ tictactoe.check_for_valid_move(game_board) }.to output(expected_message).to_stdout
+    expect { tictactoe.check_for_valid_move(game_board) }.to output(expected_message).to_stdout
   end
 
-  it "if player input is not valid, ask player to input again and again until got a valid input" do
-    game_board = "000000000"
-    
+  it 'if player input is not valid, ask player to input again and again until got a valid input' do
+    game_board = '000000000'
+
     tictactoe = Tictactoe.new
-    allow(tictactoe).to receive(:gets).and_return("10", "0", "1")
+    allow(tictactoe).to receive(:gets).and_return('10', '0', '1')
 
     expected_message = a_string_including("Sorry, your input was not valid. Can you input again?\n")
 
     expect(tictactoe).to receive(:gets).exactly(3).time
-    expect{ tictactoe.check_for_valid_move(game_board) }.to output(expected_message).to_stdout
-  end
-  
-  describe "start_game" do
-    
-    it "initiate game with user details" do
-      tictactoe = Tictactoe.new
-      expected_message = a_string_including("Enter username 1")
-      expect{tictactoe.start_game}.to output(expected_message).to_stdout
-    end
+    expect { tictactoe.check_for_valid_move(game_board) }.to output(expected_message).to_stdout
   end
 
+  describe 'start_game' do
+    it 'initiate game with user details' do
+      tictactoe = Tictactoe.new
+      expected_message = a_string_including('Enter username 1')
+      allow(tictactoe).to receive(:gets).and_return('Ted', 'Joe')
+      expect { tictactoe.start_game }.to output(expected_message).to_stdout
+    end
+  end
 end
-    
--- lib/Tictactoe.rb
+++ lib/Tictactoe.rb
@@ -1,128 +1,117 @@
 class Tictactoe
+  def start_game
+    ask_for_user_name
+    # check_for_valid_move
+  end
 
-    def start_game
-        ask_for_user_name
-        # check_for_valid_move
+  def initialize
+    @current_player = 1
+  end
+
+  def confirm_horizontal_winner(game_board)
+    if game_board[0, 3] == '111' or game_board[0, 3] == '222'
+      return true
+    elsif game_board[3, 3] == '111' or game_board[3, 3] == '222'
+      return true
+    elsif game_board[6, 3] == '111' or game_board[6, 3] == '222'
+      return true
     end
 
+    false
+  end
 
-    def initialize
-        @current_player = 1
+  def confirm_vertical_winner(game_board)
+    if game_board[0] + game_board[3] + game_board[6] == '111' or game_board[0] + game_board[3] + game_board[6] == '222'
+      return true
+    elsif game_board[1] + game_board[4] + game_board[7] == '111' or game_board[1] + game_board[4] + game_board[7] == '222'
+      return true
+    elsif game_board[2] + game_board[5] + game_board[8] == '111' or game_board[2] + game_board[5] + game_board[8] == '222'
+      return true
     end
 
-    def confirm_horizontal_winner(game_board)
-        if game_board[0, 3] == "111" or game_board[0, 3] == "222"
-            return true
-        elsif game_board[3, 3] == "111" or game_board[3, 3] == "222"
-            return true
-        elsif game_board[6, 3] == "111" or game_board[6, 3] == "222"
-            return true
-        end
-        false
+    false
+  end
+
+  def confirm_diagonal_winner(game_board)
+    if game_board[0] + game_board[4] + game_board[8] == '111' or game_board[0] + game_board[4] + game_board[8] == '222'
+      return true
+    elsif game_board[2] + game_board[4] + game_board[6] == '111' or game_board[2] + game_board[4] + game_board[6] == '222'
+      return true
     end
 
-    def confirm_vertical_winner(game_board)
-        if game_board[0] + game_board[3] + game_board[6] == "111" or game_board[0] + game_board[3] + game_board[6] == "222"
-            return true
-        elsif game_board[1] + game_board[4] + game_board[7] == "111" or game_board[1] + game_board[4] + game_board[7] == "222"
-            return true
-        elsif game_board[2] + game_board[5] + game_board[8] == "111" or game_board[2] + game_board[5] + game_board[8] == "222"
-            return true
-        end
-        false
+    false
+  end
+
+  def confirm_winner(game_board)
+    confirm_horizontal_winner(game_board) or confirm_diagonal_winner(game_board) or confirm_vertical_winner(game_board)
+  end
+
+  def display_grid(game_board)
+    # "   \n   \n   "
+    player1x = game_board.gsub('0', ' ').gsub('1', 'X').gsub('2', 'O')
+    with_newlines = player1x[0, 3] + "\n" + player1x[3, 3] + "\n" + player1x[6, 3]
+  end
+
+  def check_input(game_board, move) # return true or false
+    return false if move.is_a?(String) or move < 1 or move > 9
+
+    board_index = move - 1
+    game_board[board_index] == '0' or game_board[board_index].nil?
+  end
+
+  def ask_for_user_input
+    puts 'please input your next move ( a number from 1 to 9 )'
+    gets.chomp
+  end
+
+  def ask_for_user_name
+    puts 'Enter username 1'
+    @name1 = gets.chomp
+    puts 'Hello ' + @name1 + '!'
+    puts 'Enter username 2'
+    @name2 = gets.chomp
+    puts 'Hello ' + @name2 + '!'
+  end
+
+  # def record_game_board
+  #     puts "Enter #{@name1}'s move"
+  #     @player1move = gets.chomp
+  #     puts "Hello " + @name1 +"!"
+  #     puts "Enter username 2"
+  #     @name2 = gets.chomp
+  #     puts "Hello " + @name2 + "!"
+  # end
+
+  # def insert_move
+  #     while confirm_winner = false do
+  #     @player1_move_index = @player1_move.to_i-1
+  #         @current_player
+
+  #         # after change the board
+
+  #         if @current_player == 1
+  #             @current_player = 2
+  #         else
+  #             @current_player = 1
+  #         end
+  #     end
+  # end
+
+  def insert_move(game_board, move)
+    # insert X into game_board using board index
+    game_board[move - 1] = @current_player.to_s
+    game_board
+  end
+
+  def check_for_valid_move(game_board)
+    move = ask_for_user_input
+    until check_input(game_board, move.to_i)
+      puts 'Sorry, your input was not valid. Can you input again?'
+      move = ask_for_user_input
     end
-
-    def confirm_diagonal_winner(game_board)
-        if game_board[0] + game_board[4] + game_board[8] == "111" or game_board[0] + game_board[4] + game_board[8] == "222"
-            return true
-        elsif game_board[2] + game_board[4] + game_board[6] == "111" or game_board[2] + game_board[4] + game_board[6] == "222"
-            return true
-        end
-        false
-    end
-
-    def confirm_winner(game_board)
-        confirm_horizontal_winner(game_board) or confirm_diagonal_winner(game_board) or confirm_vertical_winner(game_board) 
-    end
-
-    def display_grid(game_board)
-        # "   \n   \n   "
-        player1x = game_board.gsub("0", " ").gsub("1","X").gsub("2","O")
-        with_newlines = player1x[0, 3] + "\n" + player1x[3, 3] + "\n" + player1x[6, 3]
-    end
-
-    def check_input(game_board, move)  # return true or false
-        if move.is_a?(String) or move < 1 or move > 9 
-            return false
-        end
-        board_index = move - 1
-        game_board[board_index] == '0' or game_board[board_index] == nil
-    end
-
-    def ask_for_user_input
-        puts "please input your next move ( a number from 1 to 9 )"
-        input = gets.chomp
-        input
-    end
-
-    def ask_for_user_name
-        puts "Enter username 1"
-        @name1 = gets.chomp
-        puts "Hello " + @name1 +"!"
-        puts "Enter username 2"
-        @name2 = gets.chomp
-        puts "Hello " + @name2 + "!"
-    end
-
-    # def record_game_board
-    #     puts "Enter #{@name1}'s move"
-    #     @player1move = gets.chomp
-    #     puts "Hello " + @name1 +"!"
-    #     puts "Enter username 2"
-    #     @name2 = gets.chomp
-    #     puts "Hello " + @name2 + "!"
-    # end
-
-
-    # def insert_move
-    #     while confirm_winner = false do
-    #     @player1_move_index = @player1_move.to_i-1
-    #         @current_player 
-        
-
-    #         # after change the board
-
-    #         if @current_player == 1
-    #             @current_player = 2
-    #         else
-    #             @current_player = 1
-    #         end
-    #     end
-    # end
-
-    def insert_move(game_board, move)
-        # insert X into game_board using board index
-        game_board[move-1] = @current_player.to_s
-        game_board
-    end
-
-    def check_for_valid_move(game_board) 
-        move = ask_for_user_input      
-        while not check_input(game_board,move.to_i) do
-            puts "Sorry, your input was not valid. Can you input again?"
-            move = ask_for_user_input 
-        end
-        return move.to_i
-        
-    end
-
-  
-
-    
-
-
-
+    move.to_i
+  end
 end
 
 # Tictactoe.new.check_for_valid_move("000000000")
-Tictactoe.new.start_game
+# Tictactoe.new.start_game
